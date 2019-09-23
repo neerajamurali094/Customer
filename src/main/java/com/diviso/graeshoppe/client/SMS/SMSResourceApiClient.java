746c9c78c18daaedce99290fd3efc6729016aeb6
sruthykc <sruthy.k.c@gmail.com>
2019-09-23 10:31:09 +0530

    Customer name fix

--- src/main/java/com/diviso/graeshoppe/client/SMS/SMSResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/SMS/SMSResourceApiClient.java
@@ -2,7 +2,7 @@ package com.diviso.graeshoppe.client.SMS;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(url="https://api.textlocal.in")
+/*@FeignClient(url="https://api.textlocal.com")*/
 public interface SMSResourceApiClient {
 
 }
